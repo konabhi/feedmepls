ffd3c56d0a30c9f93fe288334301438be69c6ab5
konabhi <abhisingh61053@gmail.com>
2026-06-13 01:11:16 +0530

    Fix formatting issues in script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function renderCarts(list) {
 
     list.forEach(cart => {
         const card = document.createElement("div");
-        card.className = "card";
+        card.className = "card-cart";
 
         card.innerHTML = `
             <h2>${cart.name}</h2>
@@ -56,7 +56,7 @@ function renderCarts(list) {
             <a
                 href="https://wa.me/${cart.whatsapp}?text=Hi%20I%20want%20to%20order"
                 target="_blank"
-                class="btn"
+                class="whatsapp_btn"
             >
                 Order on WhatsApp
             </a>
